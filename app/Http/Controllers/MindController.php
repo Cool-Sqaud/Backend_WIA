d1baed29e0d0d85e967cacc89b36002016b6daba
luckyluuk28 <luukvdstruik.lvds@gmail.com>
2023-06-21 01:43:34 +0200

    MindController? More like MindBroken

--- app/Http/Controllers/MindController.php
+++ app/Http/Controllers/MindController.php
@@ -14,13 +14,14 @@ class MindController extends Controller
 {
     public function humidityData(){
         $table = DB::table('measurement as m')
-            ->selectRaw('ROUND(AVG(m.dewp), 3) as avg_dewp, m.station, c.country, m.date , ROUND(AVG(m.temp), 3) as temp , ROUND(AVG(m.wdsp), 3)as wdsp, ROUND(AVG(m.prcp), 3) as prcp, ROUND(AVG(m.cldc), 3) as cldc')
+            ->selectRaw('ROUND(AVG(m.dewp), 1) as dewp, m.station, c.country, m.date , ROUND(AVG(m.temp), 1) as temp , ROUND(AVG(m.wdsp), 1)as wdsp, ROUND(AVG(m.prcp), 1) as prcp, ROUND(AVG(m.cldc), 1) as cldc, s.longitude, s.latitude, s.elevation')
             ->join('geolocation as g', 'm.station', '=','g.station_name')
+            ->join('station as s', 'm.station', '=', 's.name')
             ->join('country as c', 'g.country_code', '=', 'c.country_code')
             ->whereIn('g.country_code', ['AR', 'BO', 'BR', 'CL', 'CO', 'EC', 'GY', 'PY', 'PE', 'SR', 'UY', 'VE', 'GS', 'FK', 'GF'])
             ->where("m.date", Carbon::yesterday()->format('Y-m-d'))
             ->groupBy('m.station', 'm.date',  'g.country_code')
-            ->orderByRaw('m.date desc, avg_dewp desc')
+            ->orderByRaw('m.date desc, dewp desc')
             ->limit(10)
             ->get();
         return $table;
@@ -28,12 +29,13 @@ class MindController extends Controller
 
     public function humidityDataHistory(){
         $table = DB::table('measurement as m')
-            ->selectRaw('ROUND(AVG(m.dewp), 3) as avg_dewp, m.station, c.country, m.date , ROUND(AVG(m.temp), 3) as temp , ROUND(AVG(m.wdsp), 3)as wdsp, ROUND(AVG(m.prcp), 3) as prcp, ROUND(AVG(m.cldc), 3) as cldc')
+            ->selectRaw('ROUND(AVG(m.dewp), 1) as dewp, m.station, c.country, m.date , ROUND(AVG(m.temp), 1) as temp , ROUND(AVG(m.wdsp), 1)as wdsp, ROUND(AVG(m.prcp), 1) as prcp, ROUND(AVG(m.cldc), 1) as cldc, s.longitude, s.latitude, s.elevation')
             ->join('geolocation as g', 'm.station', '=','g.station_name')
+            ->join('station as s', 'm.station', '=', 's.name')
             ->join('country as c', 'g.country_code', '=', 'c.country_code')
             ->whereIn('g.country_code', ['AR', 'BO', 'BR', 'CL', 'CO', 'EC', 'GY', 'PY', 'PE', 'SR', 'UY', 'VE', 'GS', 'FK', 'GF'])
             ->groupBy('m.station', 'm.date',  'g.country_code')
-            ->orderByRaw('m.date desc, avg_dewp desc')
+            ->orderByRaw('m.date desc, dewp desc')
             ->get();
         return $table;
     }
